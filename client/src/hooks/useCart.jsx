a3b632748bbffbf7933de592c66d083307b7beca
BayajidAlam <bayajidalam2001@gmail.com>
2023-12-16 20:45:14 +0600

    custom hook created

--- client/src/hooks/useCart.jsx
+++ client/src/hooks/useCart.jsx
@@ -1,7 +1,24 @@
-import { useQuery } from '@tanstack/react-query'
-import { useContext } from 'react'
-import { AuthContext } from '../providers/AuthProvider'
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
 
 const useCart = (email) => {
-  const { user }  = useContext(AuthContext);
-}
+  const { user } = useContext(AuthContext);
+  const {
+    isLoading,
+    data: cart = [],
+    refetch
+  } = useQuery({
+    queryKey: ["cart", user?.email],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/carts?email=${user?.email}`
+      );
+      return res.json();
+    },
+  });
+
+  return [cart, isLoading,refetch];
+};
